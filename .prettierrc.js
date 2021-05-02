d578297907d09be726ee841bcb5c87efbf1235b0
cocoder16 <cocoder16@gmail.com>
2021-05-02 15:46:37 +0900

    Update README, prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid",
+  jsxSingleQuote: true,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## node.js
 
 ### no DB, this server don't save any data.
+
+### reference
+
+웹소켓 기본 개념: https://woowacourse.github.io/javable/post/2020-09-20-websocket/
