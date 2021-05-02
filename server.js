4352862f1acbd9d304a79a5984b32b5aaa8739ea
cocoder16 <cocoder16@gmail.com>
2021-05-02 17:20:20 +0900

    Install socket.io
    Add socket onjoin

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const socketIo = require("socket.io")(server);
 const cors = require("cors");
 const colors = require("colors/safe");
 // const helmet = require('helmet');
 
 const routes = require("./src/routes");
 
-const server = express();
 const port = process.env.PORT;
+// app.use(helmet());
 
-// server.use(helmet());
+app.use(cors({ origin: process.env.FRONT_URL }));
+app.use(routes);
 
-server.use(cors({ origin: process.env.FRONT_URL }));
-server.use(routes);
+socketIo.on("connection", function (socket) {
+  console.log("소켓 접속 완료");
+  console.log("### socket: ", socket);
+
+  // 참가
+  socket.on("join", userName => {
+    const roomName = "room 1";
+    socket.join(roomName);
+    console.log("### join ", socket.rooms); // Set { <socket.id>, "room 1" }
+    io.to(roomName).emit(`${userName} has joined the room`); // broadcast to everyone in the room
+  });
+
+  // //
+  // socket.on("alert", touserid => {
+  //   //alet 이벤트로 데이터 받기
+  //   io.to(touserid).emit("heejewake", touserid); //touserid: 클라이언트1이 보낸데이터"hwi"
+  // }); //heejewake이벤트: hwi 에게 메시지 hwi를 보낸다
+});
 
 server.listen(port, () => {
   console.log(
