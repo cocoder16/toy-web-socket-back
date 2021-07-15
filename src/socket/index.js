f66a162fb17d246e86007a0e9ae73c9014588a07
cocoder16 <cocoder16@gmail.com>
2021-07-16 00:46:49 +0900

    Add docs

--- src/socket/index.js
+++ src/socket/index.js
@@ -5,36 +5,37 @@ const { formatHourMin } = require("../lib/format/time");
 
 module.exports = function (socketIo) {
   socketIo.on("connection", function (socket) {
+    // TODO: connect한 유저의 IP 등 정보를 받아오기
     console.log(`${colors.brightGreen("socket connection succeeded.")}`);
     const roomName = "room 1";
 
     // 참가
-    socket.on("join", user => {
+    socket.on("JOIN", user => {
       socket.join(roomName);
-      printSocketEventFired("join", { user, room: socket.rooms });
+      printSocketEventFired("JOIN", { user, room: socket.rooms });
       const content = `${user.name} has joined the room.`;
       const time = formatHourMin(new Date());
       const data = { content, time };
-      socketIo.to(roomName).emit("receive", data);
-      printSocketEventFired("receive", { to: roomName, ...data });
+      socketIo.to(roomName).emit("RECEIVE", data);
+      printSocketEventFired("RECEIVE", { to: roomName, ...data });
     });
 
     // 메시지 송신
-    socket.on("send", ({ user, content }) => {
+    socket.on("SEND", ({ user, content }) => {
       const time = formatHourMin(new Date());
       const data = { nickname: user.name, content, time };
-      socketIo.to(roomName).emit("receive", data);
-      printSocketEventFired("receive", { to: roomName, ...data });
+      socketIo.to(roomName).emit("RECEIVE", data);
+      printSocketEventFired("RECEIVE", { to: roomName, ...data });
     });
 
     // 나가기
-    socket.on("leave", user => {
+    socket.on("LEAVE", user => {
       socket.leave(roomName);
       const content = `${user.name} has leaved the room.`;
       const time = formatHourMin(new Date());
       const data = { content, time };
-      socketIo.to(roomName).emit("receive", data);
-      printSocketEventFired("receive", { to: roomName, ...data });
+      socketIo.to(roomName).emit("RECEIVE", data);
+      printSocketEventFired("RECEIVE", { to: roomName, ...data });
     });
   });
 };
