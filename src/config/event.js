6ab4ed2c1b261a57ef7f4fe8abd290b838aee9dd
cocoder16 <cocoder16@gmail.com>
2021-07-25 17:02:59 +0900

    Add event config

---
+++ src/config/event.js
@@ -0,0 +1,8 @@
+const SOCKET_EVENT = {
+  JOIN: "JOIN",
+  SEND: "SEND",
+  RECEIVE: "RECEIVE",
+  LEAVE: "LEAVE",
+};
+
+module.exports = { SOCKET_EVENT };
--- src/socket/index.js
+++ src/socket/index.js
@@ -1,5 +1,6 @@
 const colors = require("colors/safe");
 
+const { SOCKET_EVENT } = require("../config/event");
 const printSocketEventFired = require("../lib/prints/printSocketEventFired");
 const { formatHourMin } = require("../lib/format/time");
 
@@ -10,32 +11,38 @@ module.exports = function (socketIo) {
     const roomName = "room 1";
 
     // 참가
-    socket.on("JOIN", user => {
+    socket.on(SOCKET_EVENT.JOIN, user => {
       socket.join(roomName);
-      printSocketEventFired("JOIN", { user, room: socket.rooms });
+      printSocketEventFired(SOCKET_EVENT.JOIN, { user, room: socket.rooms });
       const content = `${user.name} has joined the room.`;
       const time = formatHourMin(new Date());
       const data = { content, time };
-      socketIo.to(roomName).emit("RECEIVE", data);
-      printSocketEventFired("RECEIVE", { to: roomName, ...data });
+      socketIo.to(roomName).emit(SOCKET_EVENT.RECEIVE, data);
+      printSocketEventFired(SOCKET_EVENT.RECEIVE, { to: roomName, ...data }); // TODO: emit 이후에는 항상 로그를 출력하도록, 그리고 fire랑 구분된 메시지로,
     });
 
     // 메시지 송신
-    socket.on("SEND", ({ user, content }) => {
+    socket.on(SOCKET_EVENT.SEND, ({ user, content }) => {
       const time = formatHourMin(new Date());
       const data = { nickname: user.name, content, time };
-      socketIo.to(roomName).emit("RECEIVE", data);
-      printSocketEventFired("RECEIVE", { to: roomName, ...data });
+      printSocketEventFired(SOCKET_EVENT.SEND, { user, content });
+      socketIo.to(roomName).emit(SOCKET_EVENT.RECEIVE, data);
+      printSocketEventFired(SOCKET_EVENT.RECEIVE, { to: roomName, ...data });
     });
 
     // 나가기
-    socket.on("LEAVE", user => {
+    socket.on(SOCKET_EVENT.LEAVE, user => {
       socket.leave(roomName);
+      printSocketEventFired(SOCKET_EVENT.LEAVE, { user });
       const content = `${user.name} has leaved the room.`;
       const time = formatHourMin(new Date());
       const data = { content, time };
-      socketIo.to(roomName).emit("RECEIVE", data);
-      printSocketEventFired("RECEIVE", { to: roomName, ...data });
+      socketIo.to(roomName).emit(SOCKET_EVENT.RECEIVE, data);
+      printSocketEventFired(SOCKET_EVENT.RECEIVE, { to: roomName, ...data });
+    });
+
+    socket.on("disconnect", reason => {
+      console.log(`${colors.brightGreen("disconnect")}: ${reason}`);
     });
   });
 };
