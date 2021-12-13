0988659e8c1ca8562423c42384bf90f6e2afedae
cocoder16 <cocoder16@gmail.com>
2021-12-13 15:15:22 +0900

    [Fix] print log

--- src/lib/print.js
+++ src/lib/print.js
@@ -1,9 +1,9 @@
 function printPingLog(eventType, data) {
-  console.log(`${eventType} is received with data: ${data}`);
+  console.log(`${eventType} is received with data: ${JSON.stringify(data)}`);
 }
 
 function printPongLog(eventType, data) {
-  console.log(`${eventType} is emitted with data: ${data}`);
+  console.log(`${eventType} is emitted with data: ${JSON.stringify(data)}`);
 }
 
 module.exports = { printPingLog, printPongLog };
