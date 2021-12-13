eca43ab7216d3246dbf7ad7d48ef2b3b99419b49
cocoder16 <60813614+cocoder16@users.noreply.github.com>
2021-12-13 23:35:22 +0900

    리팩터링 (#4)
    * [Refactor] Clean files
    
    * [Refactor] socket, print
    
    [Add] .env file to remote
    
    * [Fix] print log
    
    * [Refactor] event type
    
    * [Delete] formateHourMin
    
    * [Refactor]
    
    * [Fix] socket listener

--- src/lib/prints/printResponseLog.js
+++
@@ -1,9 +0,0 @@
-const colors = require("colors/safe");
-
-module.exports = function printResponseLog({ status, error }) {
-  console.log(`Status: ${status}`);
-
-  if (error) {
-    console.log(`${colors.red("Error:")} \n`);
-  }
-};
